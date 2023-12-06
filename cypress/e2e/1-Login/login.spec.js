a5919755a8d8219621292e3d9ba63df5f841663e
jonathanrsbr <jonathanrsbr@gmail.com>
2023-12-06 15:24:30 -0300

    Adiciona modificações no package.json

--- cypress/e2e/1-Login/login.spec.js
+++ cypress/e2e/1-Login/login.spec.js
@@ -1,7 +1,14 @@
-// Ações:
-// Acessar o login
-
-// Preencher o login
-
-// Elementos:
-// id, button-login
+/// <reference types="Cypress" />
+describe('Central de Atendimento ao Cliente TAT', function() {
+    beforeEach(function() {
+      cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'); // Visiting the path to internal HTML file
+    });
+      it('Logar no OrangeHRM', function() {
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input')
+          .type('Admin')
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input')
+          .type('admin123')
+        cy.get('.oxd-button')
+          .click()
+    });
+  });
