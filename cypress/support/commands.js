a11a1489ee0fa130c49023316ce7c6cc2dccce0f
jonathanrsbr <jonathanrsbr@gmail.com>
2023-12-06 23:21:22 -0300

    Modifica os seletores command login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,6 +26,9 @@
 
 Cypress.Commands.add('login', (username, password) => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') 
+    //cy.get('input[name="username"]').clear().type(username)
+    //cy.get('input[name="password"]').clear().type(password)
+
     cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type(username)
     cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type(password)
     cy.get('.oxd-button').click()
