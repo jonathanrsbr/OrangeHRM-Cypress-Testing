2d40d434a72840346118b824a10e1723559a5da5
jonathanrsbr <jonathanrsbr@gmail.com>
2023-12-07 01:14:15 -0300

    Acrescenta a assertion ao login com sucesso

--- cypress/e2e/1-Login/login.spec.cy.js
+++ cypress/e2e/1-Login/login.spec.cy.js
@@ -2,5 +2,8 @@
 describe('Teste de Login do Administrador', () => {
   it('Logar no OrangeHRM', () => {
     cy.login('Admin', 'admin123')
+    cy.get('.oxd-sidepanel-body') // Painel lateral presente apenas no home menu
+      .should('be.visible')
+      .contains('Buzz') // Um dos campos do painel lateral
   })
 })
