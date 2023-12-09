e6231d8848fc1107c33109ad2527fe76093e3067
jonathanrsbr <jonathanrsbr@gmail.com>
2023-12-09 04:12:45 -0300

    Adiciona testes de api e formatação final

--- cypress/e2e/3-Teste-de-API/api-testing.spec.cy.js
+++ cypress/e2e/3-Teste-de-API/api-testing.spec.cy.js
@@ -1,33 +1,56 @@
 describe('Teste de API do Site Orange HRM', () => {
-  beforeEach(() => {
-    cy.api('POST', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login', {
+  // Verifica o Header
+  it('Valida o header info', () => {
+    // Verifica o header info antes dos outros testes
+    cy.api('GET', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login', {
       username: 'Admin',
       password: 'admin123'
     }).as('login')
+
+    cy.get('@login').its('headers.content-type').should('include', 'text/html; charset=UTF-8')
   })
 
   // Teste de autenticação bem-sucedida:
   it('Deve fazer login com sucesso', () => {
+    // Antes de cada teste, faz login com as credenciais padrão
+    cy.api('GET', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login', {
+      username: 'Admin',
+      password: 'admin123'
+    }).as('login')
+
     // Verifica se a resposta é bem-sucedida
     cy.get('@login').its('status').should('eq', 200)
 
     // Verifica se a resposta contém informações esperadas
-    cy.get('@login')
-      .its('body')
-      .then((body) => {
-        cy.get(body).should('include.text', 'Bem-vindo, Admin')
-      })
+    cy.get('@login').its('body.token').should('not.exist')
   })
 
   // Teste de autenticação mal-sucedida:
-  it('Deve falhar ao fazer login com credenciais inválidas', () => {
+  it('Deve apresentar o status 404, pois credenciais apresentadas não existem', () => {
     // Tentativa de login com credenciais inválidas
-    cy.api('POST', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login', {
-      username: 'Testador',
-      password: 'senhateste'
+    cy.request({
+      method: 'POST',
+      url: 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/validateCredentials',
+      failOnStatusCode: false,
+      body: {
+        username: 'TestadorErrado',
+        password: 'senhaErrada1'
+      }
     }).as('badLogin')
 
     // Verifica se a resposta indica uma falha de autenticação
-    cy.get('@badLogin').its('status').should('eq', 401) // 401 - Unauthorized
+    cy.get('@badLogin').its('status').should('eq', 404) // 404 - Not found
+  })
+
+  // Teste de autenticação alternativa, Bem-sucedida:
+  it('Deve fazer login com credenciais válidas, adicionadas no campo PIM', () => {
+    // Tentativa de login com credenciais válidas
+    cy.api('GET', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login', {
+      username: 'joaosilva75',
+      password: 'silva150275'
+    }).as('outroLogin')
+
+    // Verifica se a resposta indica credenciais autorizadas
+    cy.get('@outroLogin').its('status').should('eq', 200) // 200 - Credenciais autorizadas
   })
 })
