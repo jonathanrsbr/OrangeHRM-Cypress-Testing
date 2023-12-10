4047bc82f85662abfcd1a42ad70a02b717344a72
jonathanrsbr <jonathanrsbr@gmail.com>
2023-12-09 22:25:19 -0300

    Cria custom command para cadastro de usuário e refatora campo PIM

--- cypress/e2e/2-Campo-PIM/register-employee.spec.cy.js
+++ cypress/e2e/2-Campo-PIM/register-employee.spec.cy.js
@@ -1,170 +1,148 @@
 /// <reference types="Cypress" />
-describe('Testes de aplicações no PIM', () => {
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false
+})
+
+describe('Testes de cadastro de colaborador no campo PIM', () => {
   beforeEach(() => {
-    cy.login('Admin', 'admin123')
+    cy.login('Admin', 'admin123') // realiza o login
+    cy.get('span.oxd-text').contains('PIM').click() // clica em PIM
+    cy.get('.orangehrm-header-container > .oxd-button').click() // clica no botão "Add"
   })
 
-  it('Realizar cadastro de colaborador com username com menos de 5 caracteres', () => {
-    cy.contains('PIM').click()
-    cy.get('.orangehrm-header-container > .oxd-button').click()
-    cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').clear().type('João')
-    cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').clear().type('Bezerra')
-    cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').clear().type('Silva')
-    cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('0468875')
-    cy.get('.oxd-switch-input').click()
-    cy.get(':nth-child(4) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('joao')
-    cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silva150275')
-    cy.get('.oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silva150275')
-    cy.get('.oxd-button--secondary').click()
-    cy.get('.oxd-input-group > .oxd-text').should('contains.text', 'Should be at least 5 characters')
+  it('Cadastro usando username com menos de 5 caracteres', () => {
+    cy.preencherFormularioCadastro({
+      firstName: 'João',
+      middleName: 'Bezerra',
+      lastName: 'Silva',
+      id: '0468875',
+      username: 'joao',
+      password: 'silva150275'
+    })
+    cy.get('.oxd-input-group > .oxd-text').should('be.visible').contains('5 characters')
   })
 
-  it('Realizar cadastro de colaborador com username com mais de 40 caracteres', () => {
-    cy.contains('PIM').click()
-    cy.get('.orangehrm-header-container > .oxd-button').click()
-    cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').clear().type('João')
-    cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').clear().type('Bezerra')
-    cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').clear().type('Silva')
-    cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('0468875')
-    cy.get('.oxd-switch-input').click()
-    cy.get(':nth-child(4) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('joaobezerradasilva15021975198666777898977')
-    cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silva150275')
-    cy.get('.oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silva150275')
-    cy.get('.oxd-button--secondary').click()
+  it('Cadastro usando username com mais de 40 caracteres', () => {
+    cy.preencherFormularioCadastro({
+      firstName: 'João',
+      middleName: 'Bezerra',
+      lastName: 'Silva',
+      id: '0468875',
+      username: 'joaobezerradasilva15021975198666777898977',
+      password: 'silva150275'
+    })
     cy.get('.oxd-input-group > .oxd-text').should('contains.text', 'Should not exceed 40 characters')
   })
 
-  it('Realizar cadastro de colaborador com Full Name contendo números e caracteres especiais', () => {
-    cy.contains('PIM').click()
-    cy.get('.orangehrm-header-container > .oxd-button').click()
-    cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').clear().type('João6541564@')
-    cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').clear().type('Bezerra)){}')
-    cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').clear().type('Silva:::.|')
-    cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('0468876')
-    cy.get('.oxd-switch-input').click()
-    cy.get(':nth-child(4) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('joaosilva76')
-    cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silva150275')
-    cy.get('.oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silva150275')
-    cy.get('.oxd-button--secondary').click()
+  it('Cadastro de colaborador com Full Name contendo números e caracteres especiais', () => {
+    cy.preencherFormularioCadastro({
+      firstName: 'João6541564@',
+      middleName: 'Bezerra)){}',
+      lastName: 'Silva:::.|',
+      id: '0468875',
+      username: 'joaosilva76',
+      password: 'silva150275'
+    })
     cy.get('.oxd-toast').should('contains.text', 'Saved')
   })
 
   it('Realizar cadastro com campo de Full Name vazio', () => {
-    cy.contains('PIM').click()
-    cy.get('.orangehrm-header-container > .oxd-button').click()
-    cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('0468875')
-    cy.get('.oxd-switch-input').click()
-    cy.get(':nth-child(4) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('joaosilva75')
-    cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silva150275')
-    cy.get('.oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silva150275')
-    cy.get('.oxd-button--secondary').click()
+    cy.preencherFormularioCadastro({
+      firstName: '',
+      middleName: '',
+      lastName: '',
+      id: '0468875',
+      username: 'joaosilva76',
+      password: 'silva150275'
+    })
     cy.get('.--name-grouped-field > :nth-child(1) > .oxd-text').should('have.text', 'Required')
     cy.get('.--name-grouped-field > :nth-child(2)').should('have.text', 'Required')
     cy.get('.--name-grouped-field > :nth-child(3) > .oxd-text').should('have.text', 'Required')
   })
 
   it('Realizar cadastro de colaborador com Password contendo apenas letras', () => {
-    cy.contains('PIM').click()
-    cy.get('.orangehrm-header-container > .oxd-button').click()
-    cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').clear().type('João')
-    cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').clear().type('Bezerra')
-    cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').clear().type('Silva')
-    cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('0468875')
-    cy.get('.oxd-switch-input').click()
-    cy.get(':nth-child(4) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('joaosilva75')
-    cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silvajoaosilva')
-    cy.get('.oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silvajoaosilva')
-    cy.get('.oxd-button--secondary').click()
+    cy.preencherFormularioCadastro({
+      firstName: 'João',
+      middleName: 'Bezerra',
+      lastName: 'Silva',
+      id: '0468875',
+      username: 'joaosilva76',
+      password: 'silvajoaosilva'
+    })
     cy.get('.oxd-input-group > .oxd-text').should('contains.text', 'Your password must contain minimum 1 number')
   })
-
   it('Realizar cadastro de colaborador com Password contendo apenas números', () => {
-    cy.contains('PIM').click()
-    cy.get('.orangehrm-header-container > .oxd-button').click()
-    cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').clear().type('João')
-    cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').clear().type('Bezerra')
-    cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').clear().type('Silva')
-    cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('0468875')
-    cy.get('.oxd-switch-input').click()
-    cy.get(':nth-child(4) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('joaosilva75')
-    cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('15021975')
-    cy.get('.oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('15021975')
-    cy.get('.oxd-button--secondary').click()
+    cy.preencherFormularioCadastro({
+      firstName: 'João',
+      middleName: 'Bezerra',
+      lastName: 'Silva',
+      id: '0468875',
+      username: 'joaosilva75',
+      password: '15021975'
+    })
     cy.get('.oxd-input-group > .oxd-text').should('contains.text', 'Your password must contain minimum 1 lower-case letter')
   })
 
   it('Realizar cadastro de colaborador com Password contendo menos de 7 caracteres', () => {
-    cy.contains('PIM').click()
-    cy.get('.orangehrm-header-container > .oxd-button').click()
-    cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').clear().type('João')
-    cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').clear().type('Bezerra')
-    cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').clear().type('Silva')
-    cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('0468875')
-    cy.get('.oxd-switch-input').click()
-    cy.get(':nth-child(4) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('joaosilva75')
-    cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silva1')
-    cy.get('.oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silva1')
-    cy.get('.oxd-button--secondary').click()
+    cy.preencherFormularioCadastro({
+      firstName: 'João',
+      middleName: 'Bezerra',
+      lastName: 'Silva',
+      id: '0468875',
+      username: 'joaosilva75',
+      password: 'silva1'
+    })
     cy.get('.oxd-input-group > .oxd-text').should('contains.text', 'Should have at least 7 characters')
   })
 
   it('Realizar cadastro de colaborador com Password contendo mais de 64 caracteres', () => {
-    cy.contains('PIM').click()
-    cy.get('.orangehrm-header-container > .oxd-button').click()
-    cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').clear().type('João')
-    cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').clear().type('Bezerra')
-    cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').clear().type('Silva')
-    cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('0468875')
-    cy.get('.oxd-switch-input').click()
-    cy.get(':nth-child(4) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('joaosilva75')
-    cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('11111222223333344444!JoãoBezerraSilva1234567890J040n@B3z3rr4S1lv4')
-    cy.get('.oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('11111222223333344444!JoãoBezerraSilva1234567890J040n@B3z3rr4S1lv4')
-    cy.get('.oxd-button--secondary').click()
+    cy.preencherFormularioCadastro({
+      firstName: 'João',
+      middleName: 'Bezerra',
+      lastName: 'Silva',
+      id: '0468875',
+      username: 'joaosilva75',
+      password: '11111222223333344444!JoãoBezerraSilva1234567890J040n@B3z3rr4S1lv4'
+    })
     cy.get('.oxd-input-group > .oxd-text').should('contains.text', 'Should not exceed 64 characters')
   })
 
   it('Realizar cadastro de colaborador com Password divergente do campo confirm password', () => {
-    cy.contains('PIM').click()
-    cy.get('.orangehrm-header-container > .oxd-button').click()
-    cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').clear().type('João')
-    cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').clear().type('Bezerra')
-    cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').clear().type('Silva')
-    cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('0468875')
-    cy.get('.oxd-switch-input').click()
-    cy.get(':nth-child(4) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('joaosilva75')
-    cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silva150275')
+    cy.preencherFormularioCadastro({
+      firstName: 'João',
+      middleName: 'Bezerra',
+      lastName: 'Silva',
+      id: '0468875',
+      username: 'joaosilva75',
+      password: 'silva150275'
+    })
     cy.get('.oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silva150286')
     cy.get('.oxd-button--secondary').click()
     cy.get('.oxd-input-group > .oxd-text').should('contains.text', 'Passwords do not match')
   })
 
   it('Realizar cadastro de colaborador', () => {
-    cy.contains('PIM').click()
-    cy.get('.orangehrm-header-container > .oxd-button').click()
-    cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').clear().type('João')
-    cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').clear().type('Bezerra')
-    cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').clear().type('Silva')
-    cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('0468875')
-    cy.get('.oxd-switch-input').click()
-    cy.get(':nth-child(4) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('joaosilva75')
-    cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silva150275')
-    cy.get('.oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silva150275')
-    cy.get('.oxd-button--secondary').click()
+    cy.preencherFormularioCadastro({
+      firstName: 'João',
+      middleName: 'Bezerra',
+      lastName: 'Silva',
+      id: '0468875',
+      username: 'joaosilva75',
+      password: 'silva150275'
+    })
     cy.get('.oxd-toast').should('contains.text', 'Saved')
   })
 
   it('Realizar cadastro de colaborador já cadastrado', () => {
-    cy.contains('PIM').click()
-    cy.get('.orangehrm-header-container > .oxd-button').click()
-    cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').clear().type('João')
-    cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').clear().type('Bezerra')
-    cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').clear().type('Silva')
-    cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('0468875')
-    cy.get('.oxd-switch-input').click()
-    cy.get(':nth-child(4) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('joaosilva75')
-    cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silva150275')
-    cy.get('.oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('silva150275')
-    cy.get('.oxd-button--secondary').click()
+    cy.preencherFormularioCadastro({
+      firstName: 'João',
+      middleName: 'Bezerra',
+      lastName: 'Silva',
+      id: '0468875',
+      username: 'joaosilva75',
+      password: 'silva150275'
+    })
     cy.get('.oxd-input-group > .oxd-text').should('contains.text', 'Employee Id already exists', 'Username already exists')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -30,3 +30,19 @@ Cypress.Commands.add('login', (username, password) => {
   cy.get('input[name="password"]').clear().type(password)
   cy.get('.oxd-button').click()
 })
+
+Cypress.Commands.add('preencherFormularioCadastro', ({ firstName, middleName, lastName, id, username, password }) => {
+  //cy.get(':nth-child(2) > .oxd-main-menu-item').contains('PIM').click() // Campo PIM
+  //cy.get('.orangehrm-header-container > .oxd-button').click() // Botão Add
+
+  //cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input') // First name
+  cy.get('input[name="firstName"].orangehrm-firstname').type(firstName)
+  cy.get('input[name="middleName"].orangehrm-middlename').type(middleName)
+  cy.get('input[name="lastName"].orangehrm-lastname').type(lastName)
+  cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type(id)
+  cy.get('.oxd-switch-input').click() // Botão para abrir campo suspenso de registro de usuário e senha
+  cy.get(':nth-child(4) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').type(username)
+  cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').type(password) // Password
+  cy.get('.oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(password) // Confirm Password
+  cy.get('.oxd-button--secondary').click() // Botão Salvar
+})
